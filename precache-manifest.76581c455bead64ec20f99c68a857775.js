0fc9603241c3f19774ac7dc4d15ca52fa32a8e00
Alekskarb <korbutowski@gmail.com>
2019-12-18 13:35:00 +0300

    Updates

---
+++ precache-manifest.76581c455bead64ec20f99c68a857775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed62a360094097570dcc1b32faca5432",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "244f26ea0fb6b758ab1f",
+    "url": "/redux/static/css/main.3aff9ab5.chunk.css"
+  },
+  {
+    "revision": "28d5c1680e2ef375f6d6",
+    "url": "/redux/static/js/2.83518046.chunk.js"
+  },
+  {
+    "revision": "244f26ea0fb6b758ab1f",
+    "url": "/redux/static/js/main.74f2c93d.chunk.js"
+  },
+  {
+    "revision": "2201a963c74b28047bdf",
+    "url": "/redux/static/js/runtime~main.3027c944.js"
+  }
+]);
